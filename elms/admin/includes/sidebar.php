d800ed78de1a2f4ee595eef8c0e47c8ca46cb0ff
Zouhardya <85653967+Zouhardya@users.noreply.github.com>
2022-01-06 14:49:19 +0530

    Initial commit

---
+++ elms/admin/includes/sidebar.php
@@ -0,0 +1,75 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><span class="card-title" style="font-size:20px;">Dashboard</span></a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons"></i><span class="card-title" style="font-size:20px;">Department</span></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Add Department</a></li>
+                                <li><a href="managedepartments.php">Manage Department</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons"></i><span class="card-title" style="font-size:20px;">Leave Type</span></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Add Leave Type</a></li>
+                                <li><a href="manageleavetype.php">Manage Leave Type</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons"></i><span class="card-title" style="font-size:20px;">Students</span></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Add Student</a></li>
+                                <li><a href="manageemployee.php">Manage Student</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons"></i><span class="card-title" style="font-size:20px;">Leave Management</span></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">All Leaves </a></li>
+                                <li><a href="pending-leavehistory.php">Pending Leaves </a></li>
+                                <li><a href="approvedleave-history.php">Approved Leaves</a></li>
+                                  <li><a href="notapproved-leaves.php">Not Approved Leaves</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons"></i><span class="card-title" style="font-size:20px;">Sign Out</span></a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    <p class="copyright"><a href="http://localhost/home/">Home</a>©</p>
+                
+                </div>
+                </div>
+            </aside>
